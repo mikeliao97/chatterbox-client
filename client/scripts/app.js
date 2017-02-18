a90dd84499e5fadfb477b7a389894f46095e9d6c
mikeliao97 <mikeliao97@gmail.com>
2017-02-17 18:29:41 -0800

    Implement Functionality to Send & Recieve Messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,15 +2,15 @@
 var app = {};
 
 app.init = function() {
-  
+  this.handleSubmit();
+  this.fetch();
 };
 
 app.send = function(message) {
   $.ajax({
 	// This is the url you should use to communicate with the parse API server.
-	url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+	url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
 	type: 'POST',
-	data: JSON.stringify(message),
 	contentType: 'application/json',
 	success: function (data) {
 	  console.log('chatterbox: Message sent');
@@ -25,10 +25,20 @@ app.send = function(message) {
 app.fetch = function() {
   $.ajax({
 	// This is the url you should use to communicate with the parse API server.
-	//url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+	url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  limit: 1000,
 	type: 'GET',
+  data: {'order': '-createdAt'},
+    datatype: 'jsonp',
 	success: function (data) {
-	  console.log('chatterbox: Message sent');
+    console.log(data);
+	  // for (var key in data) {
+      // .renderMessage(data);
+    // }.
+
+    for (var a = 0; a < data.results.length; a++) {
+      app.renderMessage(data.results[a]);
+    }
 	},
 	error: function (data) {
 	  // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,4 +49,36 @@ app.fetch = function() {
 
 app.clearMessages = function() {
   $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  var username = message.username;
+  var text = message.text;
+  var userStr = "<a class='username'>" + username + "</a>";
+  var messageStr = "<p>" + text + "</p>";
+  var resultStr = userStr + messageStr;
+  $('#chats').append(resultStr);
+  // var node = document.getElementById('#chats');
+  // node.appendChild(message);
+};
+
+app.renderRoom = function(room) {
+  $('#roomSelect').append('<p>' + room + '</p>');
+};
+
+app.handleSubmit = function() {
+  var message = $('#message')[0].value;
+  var button = $('#button')[0];
+  var username = window.location.search;
+  var room = $('#rooms')[0].value;
+  
+  var finalMessage = {username: username, text: message, roomname: room};
+
+  $('#button').click(function() {
+    app.send(finalMessage);
+    console.log("Button clicked!!");
+    // app.handleSubmit();
+  });
+
+  console.log("submitted!!!!!");
 };
